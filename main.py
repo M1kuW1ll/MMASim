6f15087e959caf971886127231cec025b1ef482a
Fei Wu <94622915+M1kuW1ll@users.noreply.github.com>
2024-05-27 01:15:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -216,24 +216,27 @@ class Auction(Model):
 
         self.schedule.step()
 
-        # Select the winner of the step
+        # select the winner of the step
         if self.current_bids :
-            max_bid, aggregated_signal_at_time_of_bid = max(self.current_bids, key=lambda x : x[0])
-            self.max_bids.append(max_bid)
-            winner_id = self.bid_agents[self.current_bids.index((max_bid, aggregated_signal_at_time_of_bid))]
+            max_bid_value = max(bid for bid, _ in self.current_bids)
+            # Get all bids that are equal to the maximum bid value
+            max_bids = [(bid, agent_id) for bid, agent_id in zip(self.current_bids, self.bid_agents) if
+                        bid[0] == max_bid_value]
+            # Randomly select one of the maximum bids
+            selected_bid, winner_id = random.choice(max_bids)
+            self.max_bids.append(selected_bid[0])
             self.winning_agents.append(winner_id)
-
-            if self.aggregated_signal_max == 0 :
+            self.winner_profit = selected_bid[1]-selected_bid[0]
+            if self.aggregated_signal_max == 0:
                 self.auction_efficiency = 0
-            else :
-                self.auction_efficiency = max_bid / self.aggregated_signal_max
+            else:
+                self.auction_efficiency = selected_bid[0]/self.aggregated_signal_max
 
-            for agent in self.schedule.agents :
-                if agent.unique_id == winner_id :
-                    self.winner_profit = aggregated_signal_at_time_of_bid - max_bid
-                    self.winner_trueprofit = aggregated_signal_at_time_of_bid - max_bid - agent.pm
-                    self.winner_aggregated_signal = aggregated_signal_at_time_of_bid
+            for agent in self.schedule.agents:
+                if agent.unique_id == winner_id:
+                    self.winner_aggregated_signal = selected_bid[1]
                     self.winner_probability = agent.probability
+
 
         self.show_current_bids = self.current_bids.copy()
         self.show_bid_agents = self.bid_agents.copy()
